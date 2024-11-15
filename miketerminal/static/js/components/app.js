9c5ed16d3c1374e4840e2b4b2b49b8fdd329cdc1
macoco111 <mclovearisa@gmail.com>
2024-11-16 00:47:28 +0700

    Add files via upload

---
+++ miketerminal/static/js/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ARGGame from './components/ARGGame';
+
+function App() {
+    return (
+        <div className="App">
+            <ARGGame />
+        </div>
+    );
+}
+
+export default App;
